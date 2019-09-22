ab035aaffcd3ed31b1f2689b912a6f28538c1449
Tenzin Ngawang <tenzin.ngawang0@gmail.com>
2019-09-22 17:05:44 -0400

    Modularized card-router & list-router

--- src/card/card-router.js
+++ src/card/card-router.js
@@ -1,25 +1,99 @@
 //modularizing our code 
 const {express} = require('express');
+const uuid = require('uuid/v4');
+const logger = require('../logger');
+const {cards, lists} = require('../store');
+
 
 const cardRouter = express.Router();
 const bodyParser = express.json();
 
+//create a cards 
+
+
 cardRouter
     .route('/card')
     .get((req, res) =>{
-
+        res
+    .json(cards);
     })
     .post(bodyParser, (req, res) => { 
-        
+        const {title, content} = req.body;
+
+//validate for title & content 
+if(!title) {
+  logger.error(`Title is required`)
+  res
+    .status(400)
+    .send('Invalid data')
+}
+
+if(!content) {
+  logger.error(`Content is require`)
+  res
+    .status(400)
+    .send('Invalid data')
+}
+
+//generate ID if title & content exists & add to the cards array
+const id = uuid();
+
+const card = {
+  id, 
+  title,
+  content
+}
+cards.push(card)
+
+//logging the creation of a new card & a location header
+logger.info(`Card with ${id} was created`);
+
+res
+  .status(201)
+  .location(`http://localhost:8000/card/${id}`)
+  .json(card);
     })
 
 cardRouter
     .route('/card/:id')
     .get((req, res => {
-
+        const {id} = req.params;
+        const card = cards.find(c => c.id == id)
+      
+        if (!card) {
+          logger.error(`Card with id ${id} not found.`); //res returned in terminal & ./info.log file, created with winston
+          return res
+            .status(404) 
+            .send('Card Not Found'); //logged in postman response
+        }
+        res.json(card);
     }))
     .delete(bodyParser, (req, res) => {
+        const { id } = req.params;
 
+  const cardIndex = cards.findIndex(c => c.id == id);
+
+  if (cardIndex === -1) {
+    logger.error(`Card with id ${id} not found.`);
+    return res
+      .status(404)
+      .send('Not found');
+  }
+ 
+//remove card from lists
+  //assume cardIds are not duplicated in the cardIds array
+  lists.forEach(list => {
+    const cardIds = list.cardIds.filter(cid => cid !== id);
+    list.cardIds = cardIds;
+  });
+
+  cards.splice(cardIndex, 1);
+
+  logger.info(`Card with id ${id} deleted.`);
+
+  res
+    .status(204)
+    .end();
     })
 
     module.exports = cardRouter
---
+++ src/list/list-router.js
@@ -0,0 +1,102 @@
+const {express} = require('express');
+const uuid = require('uuid/v4');
+const logger = require('../logger');
+const {cards, lists} = require('../store');
+
+
+const listRouter = express.Router();
+const bodyParser = express.json();
+
+listRouter 
+    .route('/list')
+    .get((req, res) => {
+        res
+            .json(lists);
+    })
+    .post(bodyParser, (req, res) => {
+        const { header, cardIds = [] } = req.body;
+
+  if (!header) {
+    logger.error(`Header is required`);
+    return res
+      .status(400)
+      .send('Invalid data');
+  }
+
+  // check card IDs
+  if (cardIds.length > 0) {
+    let valid = true;
+     cardIds.forEach(cid => {
+      const card = cards.find(c => c.id == cid);
+      if (!card) {
+        logger.error(`Card with id ${cid} not found in cards array.`);
+        valid = false;
+      }
+    });
+
+    if (!valid) {
+      return res
+        .status(400)
+        .send('Invalid data');
+    }
+  }
+
+  // get an id 
+  const id = uuid();
+
+  const list = {
+    id,
+    header,
+    cardIds
+  };
+
+  lists.push(list);
+
+  logger.info(`List with id ${id} created`);
+
+  res
+    .status(201)
+    .location(`http://localhost:8000/list/${id}`)
+    .json({id});
+    })
+
+listRouter 
+    .route('/list/:id')
+    .get((req, res) => {
+        const {id} = req.params;
+  const list = lists.find(l => l.id == id)
+
+  if(!list) {
+    logger.error(`Cannot find list of ${id}`)
+    res
+      .status(404)
+      .send('List not found')
+  }
+  res.json(list)
+    })
+    .delete(bodyParser, (req, res) => {
+        const {id} = req.params;
+
+  //findIndex locates the index of the matching id value and stores in listIndex
+  //if found listIndex = value of the the Index of first match from the function 
+  //otherwise it returns -1 which is used in the validation step
+  const listIndex = lists.findIndex(li => li.id == id);
+
+  //validate if listIndex exists
+  if (listIndex === -1) {
+    logged.error(`List with ${id} not found`)
+    .status(400)
+    .send('Not found')
+  }
+//IF id matched delete it. splice(a, b) a is the index value of the element to be removed
+//b is the number of elements to remove, in this case only 1
+  lists.splice(listIndex, 1)
+
+  logger.info(`List with id ${id} deleted`)
+  res
+    .status(204)
+    .end()
+    })
+
+    module.exports = listRouter
+    
